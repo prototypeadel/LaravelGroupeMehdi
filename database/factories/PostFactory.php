b6da5c09fbc5f69056e097375da1f369c3a83cfb
adel el amrani <fanasolo@hotmail.com>
2018-05-31 09:36:26 +0200

    je me suis arreter aux factories

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,10 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Post::class, function (Faker $faker) {
+    return [
+        'titre' => $faker->sentence(rand(4,8)),
+        'contenu' => $faker->paragraph(rand(10,15)),
+    ];
+});
